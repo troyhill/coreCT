57e3b06c91414a1986ee08ad71fb25893a40d1c3
Troy Hill <hill.troy@gmail.com>
2017-03-03 17:48:21 -0500

    Update rootSize.R

--- R/rootSize.R
+++ R/rootSize.R
@@ -8,6 +8,7 @@
 #' class.names = diameter,
 #' thickness = 0.625,
 #' airHU = -850.3233,
+#' airSD = 77.6953,
 #' waterHU = 63.912,
 #' waterSD = 14.1728)
 #' 
@@ -16,9 +17,10 @@
 #' @param diameter an integer vector of diameter cut points. Units are mm (zero is added in automatically).
 #' @param class.names not used presently
 #' @param thickness CT image thickness (mm)
-#' @param airHU mean value for air-filled calibration rod (Hounsfield Units)
-#' @param waterHU mean value for water-filled calibration rod (Hounsfield Units)
-#' @param waterSD standard deviation for water-filled calibration rod (Hounsfield Units)
+#' @param airHU mean value for air-filled calibration rod (all rod arguments are in Hounsfield Units)
+#' @param airSD standard deviation for air-filled calibration rod
+#' @param waterHU mean value for water-filled calibration rod
+#' @param waterSD standard deviation for water-filled calibration rod
 #' 
 #' @return value \code{rootSize} returns a dataframe with one row per CT slice. Values returned are the number and surface area of particles in each size class with an upper bound defined in \code{diameter}.
 #' 
@@ -41,6 +43,7 @@
 #' }
 #' 
 #' @importFrom raster freq
+#' @importFrom raster match
 #' @importFrom raster boundaries
 #' @importFrom raster clump
 #' @importFrom utils setTxtProgressBar
@@ -50,67 +53,70 @@
 #' 
 #' @export
 
-# library(raster)
-
 rootSize <- function (mat.list, pixelA, 
                       diameter = c(1, 2, 5, 10, 20), # mm, targets clumps less than or equal to "diameter" argument
                       class.names = diameter,
-          thickness = 0.625, # all in mm
-          airHU = -850.3233,
-          waterHU = 63.912,
-          waterSD = 14.1728) {
+                      thickness = 0.625, # mm
+                      airHU = -850.3233, # Hounsfield Units
+                      airSD = 77.6953,
+                      waterHU = 63.912,
+                      waterSD = 14.1728) {
   # function creates dataframe with root/rhizome numbers and perimeter(!) for each depth interval
   pb <- utils::txtProgressBar(min = 0, max = length(mat.list), initial = 0, style = 3)
   voxelVol <- pixelA * thickness / 1e3 # cm3
-  water.LB <- waterHU - waterSD
+  air.UB <- airHU + airSD # lower bound on R&R, exclusive. earl adds 1 to this quantity to start next category
+  water.LB <- waterHU - waterSD # upper bound on R&R, inclusive
   # clump IDs extracted on basis of area
+  # with rule: clumps must be greater than 1 pixel
+  # this rule is based on comparison of ImageJ and R results
   diams <- c(0, diameter) # revise diameter classes to include zero
   thresh.A <- pi*(diams/2)^2 # convert diameter to area
   thresh.pixels <- round(thresh.A / pixelA)  # convert area to no of pixels (irregular clumps are included)
+  thresh.pixels[1] <- 1
   
   for (i in 1:length(mat.list)) {
     depth <- thickness * i / 10 # cm
     temp <- mat.list[[i]]
-    temp[(temp > airHU) & (temp < water.LB)] <- 1 # isolate pixels with R&R density, change to 1
+    temp[(temp > air.UB) & (temp <= water.LB)] <- 1 # isolate pixels with R&R density, change to 1
     temp[!temp == 1] <- NA # make sure only two values exist: 1, NA
     
     if (length(!is.na(temp)) > 0) {
-        rmat <- raster::raster(temp)
-        rmat.int <- raster::clump(rmat, directions = 8, gaps = FALSE)
-        clump.sub1 <- data.frame(freq(rmat.int))
-
-        for (j in 2:length(diams)) {
-          # select clumps with less than x pixels
-          clump.sub <- clump.sub1[ (clump.sub1$count <= thresh.pixels[j]) & 
-                                     (clump.sub1$count > thresh.pixels[j-1]) & 
-                                     !is.na(clump.sub1$value), ] 
-          includeID <- as.vector(clump.sub$value) # record IDs from clumps which met the criteria in previous step
-          
-          # get root perimeter (then multiply by thickness to calculate external surface area)
-          # make a new raster to be sieved
-          maskSieve <- rmat.int
-          # assign NA to all clumps whose IDs are NOT found in excludeID
-          # maskSieve[!maskSieve %in% includeID] <- NA # 
-          maskSieve <- raster::match(maskSieve, includeID)
-
-          # calculate perimeter based on clump results # plot(boundaries(maskSieve, classes = FALSE, directions = 8, asNA = TRUE))
-          a2 <- raster::freq(raster::boundaries(maskSieve, classes = FALSE, directions = 8, asNA = TRUE)) # number of "1"s x pixelSide = edge length
-          if (nrow(clump.sub) > 0) {
-            a3 <- a2[[3]] * sqrt(pixelA) # one side of pixel contributes to perimeter (neglects corners; lower-bound estimate) # (sqrt(2*sqrt(pixelArea)^2) - sqrt(pixelArea)) / sqrt(pixelArea)
-          } else if (nrow(clump.sub) == 0) {
-            a3 <- 0
-          }
-          numberOfClumps <- nrow(clump.sub)
-          rootSurfaceArea <- a3 * thickness / 100 # cm2
-          # rootSurfaceVol <- rootSurfaceArea * (thickness / 10) # cm3 # tdh: probably not a meaningful parameter
-          outDatInt <- data.frame(particles = numberOfClumps, surfArea = rootSurfaceArea) #,surfaceVol = rootSurfaceVol)
-          names(outDatInt) <- paste0(names(outDatInt), ".", diams[j - 1], "-", diams[j], "mm")
-          
-          if (j == 2) {
-            outDatInt2 <- outDatInt 
-          } else {
-            outDatInt2 <- cbind(outDatInt2, outDatInt)
-          }
+      rmat <- raster::raster(temp)
+      rmat.int <- raster::clump(rmat, directions = 8, gaps = FALSE)
+      clump.sub1 <- data.frame(freq(rmat.int))
+      
+      for (j in 2:length(diams)) {
+        # select clumps with less than x pixels
+        clump.sub <- clump.sub1[ (clump.sub1$count <= thresh.pixels[j]) & 
+                                   (clump.sub1$count > thresh.pixels[j-1]) & 
+                                   !is.na(clump.sub1$value), ] #clump.sub$A <- clump.sub$count * pixelArea
+        includeID <- as.vector(clump.sub$value) # record IDs from clumps which met the criteria in previous step
+        
+        # get root perimeter (then multiply by thickness to calculate external surface area)
+        # make a new raster to be sieved
+        maskSieve <- rmat.int
+        # assign NA to all clumps whose IDs are NOT found in excludeID
+        # maskSieve[!maskSieve %in% includeID] <- NA # 
+        maskSieve <- raster::match(maskSieve, includeID)
+        
+        # calculate perimeter based on clump results # plot(boundaries(maskSieve, classes = FALSE, directions = 8, asNA = TRUE))
+        a2 <- raster::freq(raster::boundaries(maskSieve, classes = FALSE, directions = 8, asNA = TRUE)) # number of "1"s x pixelSide = edge length
+        if (nrow(clump.sub) > 0) {
+          a3 <- a2[[3]] * sqrt(pixelA) # one side of pixel contributes to perimeter (neglects corners; lower-bound estimate) # (sqrt(2*sqrt(pixelArea)^2) - sqrt(pixelArea)) / sqrt(pixelArea)
+        } else if (nrow(clump.sub) == 0) {
+          a3 <- 0
+        }
+        numberOfClumps <- nrow(clump.sub)
+        rootSurfaceArea <- a3 * thickness / 100 # cm2
+        # rootSurfaceVol <- rootSurfaceArea * (thickness / 10) # cm3 # tdh: probably not a meaningful parameter
+        outDatInt <- data.frame(particles = numberOfClumps, surfArea = rootSurfaceArea) #,surfaceVol = rootSurfaceVol)
+        names(outDatInt) <- paste0(names(outDatInt), ".", diams[j - 1], "-", diams[j], "mm")
+        
+        if (j == 2) {
+          outDatInt2 <- outDatInt 
+        } else {
+          outDatInt2 <- cbind(outDatInt2, outDatInt)
+        }
       }
     } else if (length(!is.na(temp)) == 0) { # fill in zeroes if there aren't any clumps
       for (j in 2:length(diams)) {
