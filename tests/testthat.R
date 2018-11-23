142be1272c9c76579130a63eb63bd074bae35739
thill <thill@ibis.sfnrc.nps.gov>
2018-11-23 14:36:27 -0500

    test template

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(coreCT)
+
+test_check("coreCT")
---
+++ tests/testthat/test-name.R
@@ -0,0 +1,10 @@
+context("test-name")
+library(coreCT)
+
+# test_that("fld.frq gives correct output", {
+#   expect_equal(fld.frq(2, 1:10, units = "percent"), 0.8)
+#   expect_equal(fld.frq(2, 1:10, units = "tides"), 9)
+#   expect_error(fld.frq("string", 1:10)) 
+#   expect_error(fld.frq(2, "string"))
+#   expect_error(fld.frq(2, 1:10, units = "furlongs")) 
+# })
