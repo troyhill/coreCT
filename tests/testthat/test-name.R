7321b333da1472900ff8f38d894054a37be836a3
thill <thill@ibis.sfnrc.nps.gov>
2018-11-23 15:09:38 -0500

    tests

--- tests/testthat/test-name.R
+++ tests/testthat/test-name.R
@@ -9,6 +9,15 @@ test_that("voxDims gives correct output", {
   expect_error(voxDims(directory = names(core_426))) # tough error message to trigger!
 })
 
+test_that("coreHist gives correct output", {
+  expect_equal(length(coreHist(directory = "core_426", units = "percent")), 2)
+  expect_equal(length(coreHist(directory = "core_426", units = "monkeys")), 2)
+  expect_error(coreHist(directory = "RDATA"))
+  expect_error(coreHist(directory = "RDATA.dcm"))
+  expect_error(coreHist(directory = "RDATA.dcm")) 
+  expect_error(coreHist(directory = names(core_426))) # tough error message to trigger!
+})
+
 # test_that("fld.frq gives correct output", {
 #   expect_equal(fld.frq(2, 1:10, units = "percent"), 0.8)
 #   expect_equal(fld.frq(2, 1:10, units = "tides"), 9)
